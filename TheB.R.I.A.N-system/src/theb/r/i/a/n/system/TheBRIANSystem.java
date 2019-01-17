9bb1e752bf11b88fcfb6732f0d5c428fcaf834bb
m3-welch <morgan2.welch@live.uwe.ac.uk>
2019-01-17 20:57:37 +0000

    Created the nodes for the admin page

--- TheB.R.I.A.N-system/src/theb/r/i/a/n/system/TheBRIANSystem.java
+++ TheB.R.I.A.N-system/src/theb/r/i/a/n/system/TheBRIANSystem.java
@@ -8,10 +8,22 @@ package theb.r.i.a.n.system;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.geometry.Side;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.control.TitledPane;
+import javafx.scene.layout.GridPane;
 
 /**
  *
@@ -21,22 +33,209 @@ public class TheBRIANSystem extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        //create the main tab pane that will be the basis of the whole UI
+        TabPane tabs = new TabPane();
+        //////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////
+        /////                  -TODO-                    /////
+        /////           Need to figure out how to        /////
+        /////           stop user closing tabs           /////
+        /////                  -TODO-                    /////
+        //////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////
         
-        Scene scene = new Scene(root, 300, 250);
         
-        primaryStage.setTitle("Hello World");
+        //create the 3 tabs to be used with their appropriate titles
+        Tab adminPage = new Tab("Admin");
+        Tab viewerPage = new Tab("Viewer");
+        Tab scoresheets = new Tab("Scoresheets");
+        
+        ////////////////////
+        ///              ///
+        ///    -ADMIN-   ///
+        ///              ///
+        ////////////////////
+        
+        //create a grid pane for the admin tab and set all the preferences
+        GridPane admin = new GridPane();
+        admin.setVgap(4);
+        admin.setHgap(4);
+        admin.setPadding(new Insets(5, 5, 5, 5));
+        
+        ////////////////////////////////
+        ///                          ///
+        ///       -NEW TEAM-         ///
+        ///                          ///
+        ////////////////////////////////
+        
+        //create a titled pane that will hold the new team option and make it 
+        //non collapsible 
+        TitledPane newTeam = new TitledPane();
+        newTeam.setCollapsible(false);
+        
+        //create a grid pane to hold the nodes for the new team pane and set
+        //preferences
+        GridPane newTeamGrid = new GridPane();
+        newTeamGrid.setVgap(4);
+        newTeamGrid.setHgap(4);
+        newTeamGrid.setPadding(new Insets(5, 5, 5, 5));
+        newTeam.setText("Enter a new team");
+        
+        //create the button and textfield for the user inputs
+        TextField newTeamName = new TextField("Enter a new team name");
+        Button submitTeamName = new Button("Add Team");
+        
+        
+        //add the nodes to the new team grid pane
+        newTeamGrid.add(newTeamName, 0, 0);
+        newTeamGrid.add(submitTeamName, 1, 0);
+        
+        //set the content of the new team title pane to the new team grid pane
+        newTeam.setContent(newTeamGrid);
+        
+        //add the new team title pane to the main admin grid pane
+        admin.add(newTeam, 0, 0);
+        
+        /////////////////////////////////
+        ///                           ///
+        ///       -NEW PLAYER-        ///
+        ///                           ///
+        /////////////////////////////////
+        
+        //create a titled pane that will hold the new player option and make it 
+        //non collapsible 
+        TitledPane newPlayer = new TitledPane();
+        newPlayer.setCollapsible(false);
+        
+        //create a grid pane to hold the nodes for the new player and set
+        //preferences
+        GridPane newPlayerGrid = new GridPane();
+        newPlayerGrid.setVgap(4);
+        newPlayerGrid.setHgap(4);
+        newPlayerGrid.setPadding(new Insets(5, 5, 5, 5));
+        newPlayer.setText("Enter a new player");
+        
+        //create a textfield, a dropdown for teams and a button for the new 
+        //player
+        TextField playerName = new TextField("John");
+        Button addPlayer = new Button("Add Player");
+        
+        ComboBox teamList = new ComboBox();
+        ///////////////////////////////////////////////////////
+        ///////////////////////////////////////////////////////
+        //////                 -TODO-                    //////
+        ////// This needs to add team names using a loop //////
+        //////                 -TODO-                    //////
+        ///////////////////////////////////////////////////////
+        ///////////////////////////////////////////////////////
+        
+        //Add the nodes to the new player grid pane
+        newPlayerGrid.add(playerName, 0, 0);
+        newPlayerGrid.add(teamList, 1, 0);
+        newPlayerGrid.add(addPlayer, 2, 0);
+        
+        //add the nodes to the new player titled pane
+        newPlayer.setContent(newPlayerGrid);
+        
+        //add the new player titled pane to the admin grid pane
+        admin.add(newPlayer, 0, 1);
+        
+        ///////////////////////////////////////
+        ///                                 ///
+        ///       -GENERATE FIXTURES-       ///
+        ///                                 ///
+        ///////////////////////////////////////
+        
+        //create a new titled pane that will hold the option to generate
+        //fixtures
+        TitledPane generateFixtures = new TitledPane();
+        generateFixtures.setCollapsible(false);
+        
+        //create a grid pane to hold the nodes for the generate fixtures box
+        //and set preferences
+        GridPane generateFixturesGrid = new GridPane();
+        generateFixturesGrid.setVgap(4);
+        generateFixturesGrid.setHgap(4);
+        generateFixturesGrid.setPadding(new Insets(5, 5, 5, 5));
+        generateFixtures.setText("Generate Fixtures");
+        
+        //create the nodes that will be contained in the generate fixtures box
+        Label description = new Label();
+        Label warning = new Label();
+        description.setText("This will generate a match between all teams.");
+        warning.setText("Warning: All pre-existing match information \n"
+                + "will be removed.");
+        Button generateFixturesButton = new Button("Generate Fixtures");
+
+        //add the nodes to the generate fixtures grid
+        generateFixturesGrid.add(description, 0, 0);
+        generateFixturesGrid.add(warning, 0, 1);
+        generateFixturesGrid.add(generateFixturesButton, 1, 1);
+        
+        //add the nodes to the titled pane
+        generateFixtures.setContent(generateFixturesGrid);
+        
+        //add the title pan to the admin grid
+        admin.add(generateFixtures, 0, 2);
+        
+        ///////////////////////////////////////
+        ///                                 ///
+        ///         -GENERATE STATS-        ///
+        ///                                 ///
+        ///////////////////////////////////////
+        
+        //create a new titled pane that will hold the option to generate
+        //fixtures
+        TitledPane generateStats = new TitledPane();
+        generateStats.setCollapsible(false);
+        
+        //create a grid pane to hold the nodes for the generate fixtures box
+        //and set preferences
+        GridPane generateStatsGrid = new GridPane();
+        generateStatsGrid.setVgap(4);
+        generateStatsGrid.setHgap(4);
+        generateStatsGrid.setPadding(new Insets(5, 5, 5, 5));
+        generateStats.setText("Generate Statistics");
+        
+        //create the nodes that will be contained in the generate fixtures box
+        Label auto = new Label();
+        Label info = new Label();
+        auto.setText("The stats report will be generatred automatically \n"
+                + "every 100 seconds.");
+        info.setText("You can also generate it by clicking the button \n"
+                + " on the right.");
+        Button generateStatsButton = new Button("Generate Statistics");
+
+        //add the nodes to the generate fixtures grid
+        generateStatsGrid.add(auto, 0, 0);
+        generateStatsGrid.add(info, 0, 1);
+        generateStatsGrid.add(generateStatsButton, 1, 1);
+        
+        //add the nodes to the titled pane
+        generateStats.setContent(generateStatsGrid);
+        
+        //add the title pan to the admin grid
+        admin.add(generateStats, 0, 3);
+        
+        //set the admin page contents to the admin grid pane
+        adminPage.setContent(admin);
+        
+        //add the 4 tabs to the tab pane
+        tabs.getTabs().add(adminPage);
+        tabs.getTabs().add(viewerPage);
+        tabs.getTabs().add(scoresheets);
+        
+        //set the tabs to be down the left hand side
+        tabs.setSide(Side.LEFT);
+        
+        //set the scene with the window dimensions
+        Scene scene = new Scene(tabs, 1200, 600);
+        
+        //set the title and show
+        primaryStage.setTitle("TheB.R.I.A.N. system");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
